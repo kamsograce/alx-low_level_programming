84ae5b889157971616c5578af0e8b023eb301597
kamsograce <gracekamso@gmail.com>
2022-09-09 16:37:32 -0700

    Just committed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,25 +10,25 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			if (i < J)
-			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i !=98 || j != 99)
-				{
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i == 98 && j == 99)
+					continue;
+
 					putchar(',');
 					putchar(' ');
 				}
 			}
-		}
-	}
+
 	putchar('\n');
+
 	return (0);
 }
