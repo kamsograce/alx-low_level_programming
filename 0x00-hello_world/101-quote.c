da01b7b04b4f8086b576eb9087fb01ec975489b3
kamsograce <gracekamso@gmail.com>
2022-09-09 07:55:27 -0700

    Just committed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
 /**
- *main - Entry point
- *
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main (void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-	return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	exit(1);
 }
