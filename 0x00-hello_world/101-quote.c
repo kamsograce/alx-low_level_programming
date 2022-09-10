1e686ed43a21e578b52651bb60153d933622b25c
kamsograce <gracekamso@gmail.com>
2022-09-09 18:26:52 -0700

    Just committed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
