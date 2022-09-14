eb007f2571cc2bc4459c68cd3403e88f25925390
kamsograce <gracekamso@gmail.com>
2022-09-14 01:56:16 -0700

    Just Committed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+/*
+ * File : 0-putchar.c
+ * Auth : Kamso Grace
+ */
+#include "main.h"
+/**
+ * main - prints the string "_putchar" followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+			
