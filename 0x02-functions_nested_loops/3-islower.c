3cc7cd18b734b33f1f5fc1ec72063c01ac8d0923
kamsograce <gracekamso@gmail.com>
2022-09-14 03:34:10 -0700

    Just committed

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+/*
+ * File: 3-islower.c
+ * Auth: Kamso Grace
+ */
+#include "main.h"
+/**
+ * _islower - Return 1 if letter is lowercase, 0 if not.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x =48; x < 58; x++)
+	{
+		_putchar(x);
+	}
+	_putchar(x);
+}
