d1c587a8080c024e065a27c6166a6390c0fb7383
kamsograce <gracekamso@gmail.com>
2022-09-15 05:50:12 -0700

    Just Committed

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*
+ * main - type int, print the first '98' fibonacci
+ * @void: void argument
+ *
+ * Description: prints the numbers, followed by a new line
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	long int a = 1, b = 2, c;
+	int count = 2;
+
+	printf("%lu, ", a);
+	printf("%lu", b);
+	while (count <= 95)
+	{
+		printf(", ");
+		c = a + b;
+		printf("%lu", c);
+		a = b;
+		b = c;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
