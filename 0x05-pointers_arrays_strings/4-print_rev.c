2d09ed55146793b54b877997a39199cd6c01c154
kamsograce <gracekamso@gmail.com>
2022-09-21 09:19:47 -0700

    Just Committed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line.
+ * @s: character array to be printed.
+ */
+void print_rev(char *s)
+{
+	int i = 0, j;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{ 
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
