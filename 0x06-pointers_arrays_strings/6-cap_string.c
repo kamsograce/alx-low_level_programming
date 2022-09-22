fdeb874b9afd7300bacdbe5135241fa893152495
kamsograce <gracekamso@gmail.com>
2022-09-22 11:00:03 -0700

    Just Committed

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of s string
+ * @str: string to be changed
+ * Return: string in uppercase
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{',
+	       	'}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		else
+		{
+			for (j = 0; j < 14; j++)
+				if (str[i] == separators[j])
+				{
+					if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+						str[i + 1] = str[i + 1] - 32;
+				}
+		}
+	}
+	return (str);
+}
