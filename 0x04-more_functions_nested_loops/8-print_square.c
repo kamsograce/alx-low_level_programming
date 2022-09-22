3de49c708cc31db2a76a98a73eaf1bad10a9ab6a
kamsograce <gracekamso@gmail.com>
2022-09-22 06:45:43 -0700

    Just Committed

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - print a square.
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int count, counter;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (count = 1; count <= size; count++)
+		{
+			for (counter = 1; counter <= size; counter++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
