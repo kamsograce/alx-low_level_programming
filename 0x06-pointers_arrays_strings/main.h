ff52db9280be6d5c94542ccfd3b2f9ee19ef61b4
kamsograce <gracekamso@gmail.com>
2022-09-22 10:24:15 -0700

    Just Committed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 void reverse_array(int *a, int n);
-int _strcat(char *s1, char *src);
+int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *string_toupper(char *str);
 char *cap_string(char *str);
