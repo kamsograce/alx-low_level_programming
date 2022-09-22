38ec0153f16329cc53730dffe7f768b09489f510
kamsograce <gracekamso@gmail.com>
2022-09-22 06:08:44 -0700

    Just Committed

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+nclude "main.h"
+
+/**
+ * print_most_numbers - print number 0 through 9
+ * except 2 and 4, followed by a new line
+ */
+
+void print_most_numbers(void)
+{
+	int character;
+
+	for (character = 48; character <= 57; character++)
+	{
+		if (character != 50 && character !=52)
+			_putchar(character);
+	}
+	_putchar('\n');
+}
