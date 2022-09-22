65ec753f1ae059f3b21c01a6f274db849b4245ca
kamsograce <gracekamso@gmail.com>
2022-09-22 06:23:43 -0700

    Just Committed

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print the character '_', then a new line.
+ * @n: number of times character should be printed
+ */
+
+void print_line(int n)
+{
+	int count = 1;
+
+	if (n <= 0);
+	else
+	{
+		while (count <= n)
+		{
+			_putchar(95);
+			count++;
+		}
+	}
+	_putchar('\n');
+}
