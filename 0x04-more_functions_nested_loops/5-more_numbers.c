ce81393cdbf7e64637aa1bcb18950714421862fc
kamsograce <gracekamso@gmail.com>
2022-09-22 06:18:19 -0700

    Just Committed

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0-14 ten times
+ */
+
+void more_numbers(void)
+{
+	int count = 1, x;
+
+	while (count <= 10)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			if (x >= 10)
+				_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+		}
+		count++;
+		_putchar('\n');
+	}
+}
