19552cfca274faa248ea0007d8643b98b67b3b2e
kamsograce <gracekamso@gmail.com>
2022-09-22 08:54:03 -0700

    Just Committed

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This readme file contains all the information about this project
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reverse_array(int *a, int n);
+int _strcat(char *s1, char *src);
+char *_strcat(char *dest, char *src);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *_strncat(char *dest, char *src, int n);
+void print_number(int n);
+int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
+int strcount(char str[]);
+
+#endif /* MAIN_H */
