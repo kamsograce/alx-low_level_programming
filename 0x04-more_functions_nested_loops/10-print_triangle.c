ead1ec700ae8c38b3a0ead94e4784a09da679596
kamsograce <gracekamso@gmail.com>
2022-09-23 05:26:25 -0700

    Just Committed

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle with equal height abd width
+ * @size: the specification of height and width
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+	/*
+	 * i stands for the line number
+	 * j stands for the start j of space/# for loops
+	 * k is used to determine the number of spaces
+	 */
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			k = size - i;
+			for (j = 0; j < k; j++)	/*to print spaces*/
+			{
+				_putchar(32);
+			}
+			for (j = 0; j < k; j++)	/*to print #*/
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
