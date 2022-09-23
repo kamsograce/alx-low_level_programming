55ff333e41689e360858b1251188d505adf0256b
kamsograce <gracekamso@gmail.com>
2022-09-23 03:06:04 -0700

    Just Committed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,2 +1,25 @@
 #include "main.h"
 
+/**
+ * -strcmp - comparing two srings
+ *  @s1: first string
+ *  @s2: second string
+ *  Return: return 0 if s1 and s2 are the same
+ *  another number if not
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0, diff = 0;
+
+	while (diff == 0)
+	{
+		if ((*(s1 + a) == '\0') && (*(s2 + a) == '\0'))
+			break;
+		diff = *(s1 + a) - *(s2 + a);
+		a++;
+
+	}
+
+	return (diff);
+}
