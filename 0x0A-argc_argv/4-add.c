6616bb25c253cd0f100cea3d880a21ff3ba74e78
kamso Grace <gracekamso@gmail.com>
2022-09-30 09:20:58 -0700

    Just committed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,12 +3,12 @@
 
 /**
  * main - Prints the addition of positive numbers,
- * 	followed by a new line.
+ *	followed by a new line.
  * @argc: The number of arguments passed to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: If one of the numbers contains symbols that are non-digits - 1.
- * 	Otherwise - 0.
+ *	Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
