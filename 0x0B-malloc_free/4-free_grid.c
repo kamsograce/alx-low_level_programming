779ce3b0fbdd8f1c15c8000a9a8585cd9935b3ec
kamso Grace <gracekamso@gmail.com>
2022-10-04 10:37:44 -0700

    Just Committed

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,10 +10,10 @@
  */
 void free_grid(int **grid, int height)
 {
-	if (grid != NULL && height != 0)
-	{
-		for (; height >= 0; height--)
-			free(grid[height]);
-		free(grid);
-	}
+	int index;
+
+	for (index = 0; index < height; index++)
+		free(grid[index]);
+
+	free(grid);
 }
