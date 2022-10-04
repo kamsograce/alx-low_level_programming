178f7a4b315a354c3d8d4d49882949db3a30a1b9
kamso Grace <gracekamso@gmail.com>
2022-10-04 06:59:28 -0700

    Just Committed

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -15,29 +15,26 @@ void print_buffer(char *b, int size)
 		printf("%08x: ", byte);
 		for (i = 0; i < 10; i++)
 		{
-			printf("%08x: ", byte);
-			for (i = 0; i < 10; i++)
-			{
-				if ((i + byte) >= size)
-					printf("  ");
-				else
-					printf("%02x", *(b + i + byte));
-				if ((i % 2) != 0 && i != 0)
-					printf(" ");
-			}
-			for (i = 0; i < 10; i++)
-			{
-				if ((i + byte) >= size)
-					break;
-				else if (*(b + i + byte) >= 31 && *(b + i + byte) <= 126)
-					printf("%c", *(b + i + byte));
-				else
-					printf(".");
-			}
-			if (byte >= size)
-				continue;
-			printf("\n");
+			if ((i + byte) >= size)
+				printf("  ");
+			else
+				printf("%02x", *(b + i + byte));
+			if ((i % 2) != 0 && i != 0)
+				printf(" ");
 		}
-		if (size <= 0)
-			printf("\n");
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + byte) >= size)
+				break;
+			else if (*(b + i + byte) >= 31 && *(b + i + byte) <= 126)
+				printf("%c", *(b + i + byte));
+			else
+				printf(".");
+		}
+		if (byte >= size)
+			continue;
+		printf("\n");
 	}
+	if (size <= 0)
+		printf("\n");
+}
